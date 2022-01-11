c18e9098f494da33aa619d8a97a1a1b5d2aef3fc
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-01-11 10:55:43 +0300

    actions complited commit

---
+++ src/lessons/lesson1/lesson1.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import CurrencyExchangeContainer from '../../containers/CurrencyExchangeContainer/CurrencyEContainer'
+
+// Redux
+// https://tproger.ru/translations/redux-for-beginners/ done
+// https://monsterlessons.com/project/series/redux-js-dlya-nachinayushih done
+// https://xsltdev.ru/react/redux/intro/ 404 - Not found
+
+// Typescript genetic
+// https://habr.com/ru/post/455473/ done
+// https://habr.com/ru/company/ruvds/blog/426729/ done
+
+// Typescript from zero + typescript generic (English video)
+// https://www.youtube.com/watch?v=3d9XqFmCmQw
+
+// Container component
+// https://habr.com/ru/company/ruvds/blog/446206/ done
+
+// Dispatch
+// https://react-redux.js.org/using-react-redux/connect-mapdispatch ?
+
+const Lesson1: React.FunctionComponent = () => {
+    return (
+    <React.Fragment>
+        <CurrencyExchangeContainer />
+    </React.Fragment>
+  );
+};
+
+export default Lesson1;
+
